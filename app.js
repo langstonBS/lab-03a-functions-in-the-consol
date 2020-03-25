029a326d24da9d1d821260ca1d6bd319eee8a0d9
langstonBS <langston.beckwithstanley@gmail.com>
2020-03-25 16:37:20 -0700

    step 4

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
-console.log("hello world");
-document.body.style.color = 'orange';
+ //items identifed
+ 
+ const aaa = {name: "AAA", size: "small" , amount: 40};
+ const b = {name: "b", size: "small" , amount: 13};
+ const c = {name: "c", size: "medum" , amount: 22};
+ const d = {name: "d", size: "big" , amount: 4};
+ const a = {name: "a", size: "small" , amount: 5};
+
+
+ function logOutThings(){
+    console.log('Here are your ' + bateries.length + ' things!!!!!!' );  
+ }
+
+ //name of the varable
+ const bateries = [aaa, b, c, d, a];
+
+ //nuber of items in the varable
+ logOutThings();
+
+
+ for( let i = 0; i < bateries.length; i++){
+       console.log(bateries[i]);
+       if(bateries[i].amount <= 10){
+        console.log("you need to get " + bateries[i].name);
+    }
+}
