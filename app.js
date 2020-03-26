2d661b7914f7ccf3aff2569a3c037bd7a6f5dd0d
langstonBS <langston.beckwithstanley@gmail.com>
2020-03-26 10:04:09 -0700

    made the eslintrc fix

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
  //items identifed
  
- const aaa = {name: "AAA", size: "small" , amount: 40};
- const b = {name: "b", size: "small" , amount: 13};
- const c = {name: "c", size: "medum" , amount: 22};
- const d = {name: "d", size: "big" , amount: 4};
- const a = {name: "a", size: "small" , amount: 5};
+const aaa = { name: 'AAA', size: 'small', amount: 40 };
+const b = { name: 'b', size: 'small', amount: 13 };
+const c = { name: 'c', size: 'medum', amount: 22 };
+const d = { name: 'd', size: 'big', amount: 4 };
+const a = { name: 'a', size: 'small', amount: 5 };
 
 
- function logOutThings(array){
-    console.log('Here are your ' + array.length  + ' things!!!!!!' );  
- }
+function logOutThings(array){
+    console.log('Here are your ' + array.length + ' things!!!!!!');  
+}
 
  //name of the varable
- const bateries = [aaa, b, c, d, a];
- const amoutNeed = 23; 
+const bateries = [aaa, b, c, d, a];
+const amoutNeed = 23; 
  //nuber of items in the varable
- logOutThings(bateries);
- shopingList(bateries, amoutNeed);
+logOutThings(bateries);
+shopingList(bateries, amoutNeed);
 
 
- function shopingList(array, a){
-        for( let i = 0; i < array.length; i++){
-            console.log(array[i]);
-            if(array[i].amount <= a ){
-                console.log("you need to get " + array[i].name);
-            }
+function shopingList(array, a){
+    for(let i = 0; i < array.length; i++){
+        console.log(array[i]);
+        if(array[i].amount <= a){
+            console.log('you need to get ' + array[i].name);
         }
- }
+    }
+}
 
 
  
